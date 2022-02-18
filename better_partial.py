3aed3c372abda024593c3043ad02a57485eba28c
Chris Grimm <cgrimm1994@gmail.com>
2022-02-18 15:35:32 -0500

    added in support for specifying all positionals are unsupplied with ___

--- better_partial.py
+++ better_partial.py
@@ -8,17 +8,23 @@ class AllPositionalsUnsupplied:
 
 _ = UnSupplied()
 
+___ = AllPositionalsUnsupplied()
 
 def partial(fn):
   def g(*partial_args, **partial_kwargs):
+    all_unsupplied = False
+    if any(arg is ___ for arg in partial_args):
+      assert len(partial_args) == 1
+      all_unsupplied = True
+    
     unsupplied_arg_positions = [i for i, arg in enumerate(partial_args) if arg is _]
     unsupplied_kwargs = {key: value for key, value in partial_kwargs.items() if value is _}
-    if len(unsupplied_arg_positions) == 0 and len(unsupplied_kwargs) == 0:
+    if len(unsupplied_arg_positions) == 0 and len(unsupplied_kwargs) == 0 and (not all_unsupplied):
       return fn(*partial_args, **partial_kwargs)
     @partial
     def h(*args, **kwargs):
-      nonlocal partial_args
-      partial_args = list(partial_args)
+      nonlocal partial_args, all_unsupplied
+      partial_args = list(args) if all_unsupplied else list(partial_args)
       for i, arg in zip(unsupplied_arg_positions, args):
         partial_args[i] = arg
       for key, value in kwargs.items():
