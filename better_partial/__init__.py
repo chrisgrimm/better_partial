f7f23cfd35fb50798dad37d5219d8bf6664aaa32
Chris Grimm <cgrimm1994@gmail.com>
2022-02-19 10:14:58 -0500

    restructured code to package it into a distribution

---
+++ better_partial/__init__.py
@@ -0,0 +1,8 @@
+from better_partial.better_partial import partial, _, ___
+name = 'better_partial'
+
+__all__ = [
+  'partial',
+  '_',
+  '___'
+]
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="better_partial",
+    version="1.0.2",
+    author="Christopher Grimm",
+    author_email="cgrimm1994@gmail.com",
+    description="A more intuitive way to partially apply functions in Python.",
+    long_description="A more intuitive way to partially apply functions in Python.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/chrisgrimm/better_partial",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
